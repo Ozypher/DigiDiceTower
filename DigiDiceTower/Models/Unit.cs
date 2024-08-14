bae12ce57652bda6ebc11c5d1fc952ec27c720be
Sherwin Rahimi <SRahimi@ashleyfurniture.com>
2024-08-14 17:05:36 -0400

    Preliminary changes.

---
+++ DigiDiceTower/Models/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiDiceTower.Models
+{
+    public class Unit
+    {
+        public int Movement { get; set; }
+        public int Toughness { get; set; }
+        public int LeaderScore { get; set; }
+        public int ObjectControl {  get; set; }
+        public int Wounds { get; set; }
+        public string UnitName { get; set; }
+        public string ArmyName { get; set; }
+
+    }
+}
